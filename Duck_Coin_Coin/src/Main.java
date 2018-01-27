e2d9442a92ae9be2e7a363098562475973fcd250
Tanguy PELADO <sites@tanguypelado.fr>
2018-01-28 00:13:54 +0100

    Added main

--- Duck_Coin_Coin/src/Main.java
+++ Duck_Coin_Coin/src/Main.java
@@ -1,8 +1,42 @@
+import java.util.Scanner;
 
 public class Main
 {
+	static private void affiche_menu()
+	{
+		System.out.println("1 : Générer une blockchain");
+		System.out.println("2 : Afficher la blockchain");
+		System.out.println("3 : Afficher un bloc de la blockchain");
+		System.out.println("0 : Quitter");
+	}
+
 	static public void main(String[] args)
 	{
-		System.out.println("Hello World");
+		Scanner scan = new Scanner(System.in);
+		int resultat = 0;
+		boolean quitter = false;
+		System.out.println("Bonjour.");
+		affiche_menu();
+
+		while (quitter == false)
+		{
+			resultat = scan.nextInt();
+			System.out.println(resultat);
+			switch (resultat)
+			{
+			case 1:
+				System.out.println("Vous avez choisi de générer la blockchain...");
+				break;
+			case 2:
+				System.out.println("Vous avez choisi d'afficher la blockchain...");
+				break;
+			case 3:
+				System.out.println("Vous avez choisi d'afficher un block de la blockchain...");
+				break;
+			case 0:
+				System.out.println("Goodbye. May we meet again...");
+				quitter = true;
+			}
+		}
 	}
 }
