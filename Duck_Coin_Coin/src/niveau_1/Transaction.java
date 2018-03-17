49fcec310ebaddd87b42e75769f7fa1d0d977f1c
Tanguy PELADO <univ@tanguypelado.fr>
2018-03-17 11:17:41 +0100

    add

---
+++ Duck_Coin_Coin/src/niveau_1/Transaction.java
@@ -0,0 +1,20 @@
+package niveau_1;
+import java.util.concurrent.ThreadLocalRandom;
+public class Transaction
+{
+	private String transaction;
+
+	/**
+	 * @param transaction
+	 */
+	public Transaction()
+	{
+		int max = 100;
+		this.transaction = "source-destination :"+ThreadLocalRandom.current().nextInt(0, max + 1);
+	}
+	
+	public void afficherTransaction(Transaction trans)
+	{
+		System.out.println(trans.transaction);
+	}
+}
